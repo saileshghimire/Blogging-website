d0bab84eeb64f478c2d5208c96ee1bea9fe25e43
saileshghimire <saileshghimire1@gmail.com>
2024-06-17 17:04:35 +0545

    user login/signup page complete -backend

--- backend/src/userPage/user.ts
+++ backend/src/userPage/user.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { decode, sign, verify } from 'hono/jwt'
+import { sign } from 'hono/jwt'
 
 const user = new Hono<{
     Bindings: {
@@ -11,37 +11,68 @@ const user = new Hono<{
 }>()
 
 user.post('/signup', async (c) => {
-    try{
         const prisma = new PrismaClient({
             datasourceUrl: c.env.DATABASE_URL,
         }).$extends(withAccelerate());
     
         const body = await  c.req.json();
+        try{
         const newuser = await prisma.user.create({
             data:{
                 email:body.email,
-                password: body.password,
-                name: body.name
+                password: body.password
+        
             }
         });
-        const payload = {
-            sub: newuser.id.toString(),
-        };
-    
-        const token = await sign(payload, c.env.JWT_SECRET);
+        
+        const token = await sign({ id: newuser.id }, c.env.JWT_SECRET);
         return c.json({
             token:token
         });
 
     } catch(error){
         console.error('Error during signup:', error); // Detailed log for development
+        c.status(403);
         return c.json({ error: 'Internal server error' }, 500);
     }
 
 })
 
-user.post('/signin', (c) => {
-    return c.text("signin routes")
+user.post('/signin', async (c) => {
+        const prisma = new PrismaClient({
+            datasourceUrl: c.env?.DATABASE_URL	,
+        }).$extends(withAccelerate());
+
+        const body = await c.req.json();
+        try{
+        const user = await prisma.user.findUnique({
+            where:{
+                email: body.email
+            }
+        });
+
+        if(!user){
+            c.status(403);
+            return c.json({
+                error:"Incorrect Email or password"
+            });
+        }
+
+        const payload = {
+            sub: user.id   
+          };
+        
+        
+        const token = await sign(payload, c.env.JWT_SECRET);
+	    return c.json({
+            token: token
+        });
+
+
+    } catch(error){
+        console.error('Error during sign:', error); // Detailed log for development
+        return c.json({ error: 'Internal server error' }, 500); 
+    }
 })
 
 export default user;
