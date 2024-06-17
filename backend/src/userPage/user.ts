ccf6d62e8f822d30eace299e90c21dbf7cc8fc8b
saileshghimire <saileshghimire1@gmail.com>
2024-06-17 14:27:00 +0545

    signup route

--- backend/src/userPage/user.ts
+++ backend/src/userPage/user.ts
@@ -1,9 +1,43 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
-const user = new Hono()
+const user = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    }
+}>()
 
-user.post('/signup', (c) => {
-    return c.text("signup routes")
+user.post('/signup', async (c) => {
+    try{
+        const prisma = new PrismaClient({
+            datasourceUrl: c.env.DATABASE_URL,
+        }).$extends(withAccelerate());
+    
+        const body = await  c.req.json();
+        const newuser = await prisma.user.create({
+            data:{
+                email:body.email,
+                password: body.password,
+                name: body.name
+            }
+        });
+        const payload = {
+            sub: newuser.id.toString(),
+        };
+    
+        const token = await sign(payload, c.env.JWT_SECRET);
+        return c.json({
+            token:token
+        });
+
+    } catch(error){
+        console.error('Error during signup:', error); // Detailed log for development
+        return c.json({ error: 'Internal server error' }, 500);
+    }
+
 })
 
 user.post('/signin', (c) => {
